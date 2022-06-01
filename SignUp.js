2a78311c5686fc76bade93b0c9d8ed372d8151a0
Saksham <saksham16dec@gmail.com>
2022-06-01 23:50:59 +0530

    Adding SignUp

---
+++ SignUp.js
@@ -0,0 +1,27 @@
+// SignUp from User
+const uid ="abc123";
+var fullName="Saksham Srivastava";
+var country = "India";
+var state = "UttarPradesh";
+var gender = "Male";
+var email = "abc123@example.com";
+var password = "abc123";
+var confirmPassword = "abc123";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+// user inputs 
+// //prompt alert 
+// //console.log(uid);
+// //console.log("FullName is : ",fullName);
+// *interpulations 
+// !learned something new
+console.log(`
+    with uniqueid : ${uid}
+    user is : ${fullName}
+    user gender : ${gender}
+    user country: ${country}
+    user state : ${state}
+    user email is :${email}
+    uses password :${password}
+`);
