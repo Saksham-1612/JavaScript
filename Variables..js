15e53c3029dcbfc475fdd29d4b04ead5002e671d
Saksham <saksham16dec@gmail.com>
2022-06-01 22:29:59 +0530

    Adding Variable.js

---
+++ Variables..js
@@ -0,0 +1,7 @@
+// Variables in JavaScript
+var fullName="Saksham Srivastava";
+var isLoggedIn = false;
+var loggedCount = 0;
+var payments="Credit Card";
+console.log("loggedCount",loggedCount);
+console.log("payments",payments);
