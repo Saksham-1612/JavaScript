81b700df5afd0d54bab3bc179e9cc7bf0899cd78
Saksham <saksham16dec@gmail.com>
2022-06-02 01:49:13 +0530

    Learned Conditioanals

---
+++ Coditionals.js
@@ -0,0 +1,18 @@
+var temperature;
+// TODO:Go to google and get temperature
+temperature =25;
+
+// var result =temperature<20;
+// console.log(result);
+
+// 
+if (temperature<20)
+{
+    console.log("Its cold outside!!!!");
+}
+else if (temperature<30) {
+    console.log("temperature is moderate");
+}
+else{
+    console.log("temperature is hot")
+}
