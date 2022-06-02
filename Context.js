73207d65fa16896075324ef65d739e363a678d8a
Saksham <saksham16dec@gmail.com>
2022-06-02 17:27:54 +0530

    Adding Context

---
+++ Context.js
@@ -0,0 +1,9 @@
+// sayHello();
+// function sayHello() {
+//     console.log("Hello Saksham");
+// }
+var myName = "Saksham";
+if (myName==window.myName) {
+    console.log("This is true");
+}
+// !window only works in browser not in node.js
