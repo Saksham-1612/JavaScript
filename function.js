0f85498b8c4ea0434234be137b0d1dbc38f3c430
Saksham <saksham16dec@gmail.com>
2022-06-02 17:03:36 +0530

    Adding function js

---
+++ function.js
@@ -0,0 +1,11 @@
+//  functions (🤖) 
+function sayHello(name) {
+    console.log(`Hello ${name}`);
+}
+function namastey() {
+    return "Namastey"
+    
+}
+var greet = namastey();
+console.log(greet);
+sayHello("Saksham");
