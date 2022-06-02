80b96cba8c6feebedc3789224ad5368d2b4ab5a2
Saksham <saksham16dec@gmail.com>
2022-06-02 16:36:50 +0530

    Adding Switch js

---
+++ Switch.js
@@ -0,0 +1,16 @@
+var user="admin";
+switch (user) {
+    case "admin":
+        console.log("Full Access");
+        
+        break;
+    case "testprep":
+        console.log("Acess to test");
+        break;
+    case "user":
+        console.log("Acess to view");
+        break;
+        
+    default:
+        break;
+}
