3e080784681b8bfe2c47d0a0985d6229f32d9598
Saksham <saksham16dec@gmail.com>
2022-06-04 01:30:48 +0530

    Adding Array js

---
+++ Array.js
@@ -0,0 +1,4 @@
+// ?Array helps us in declaring many properties in a continious memory
+var country =["India","USA","JAPAN","RUSSIA"];
+var states =["UP","RAJASTHAN","ASSAM"];
+console.log(country);
