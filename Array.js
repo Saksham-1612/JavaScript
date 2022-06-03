75fb13d608825960422a1bc3d98213b50d3af5f6
Saksham <saksham16dec@gmail.com>
2022-06-04 01:51:40 +0530

    Updated Array.js

--- Array.js
+++ Array.js
@@ -1,4 +1,16 @@
 // ?Array helps us in declaring many properties in a continious memory
 var country =["India","USA","JAPAN","RUSSIA"];
-var states =["UP","RAJASTHAN","ASSAM"];
+var states =new Array("UP","RAJASTHAN","ASSAM");
+// console.log(country);
+// Gives lenght of array
+console.log(country.length);
+country[1]="CHINA";
+// console.log(country[1]);
+// country.pop();
+// country.pop();
+// console.log(country);
+country.unshift("NEW VALUE");
+// console.log(country);
+// country.shift();
 console.log(country);
+console.log(Array.from("saksham"));
