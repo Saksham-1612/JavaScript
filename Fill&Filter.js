08d81700545287bb2e2bbc2a44c8530dcbb83357
Saksham <saksham16dec@gmail.com>
2022-06-04 02:33:11 +0530

    Adding Fill & Filter

---
+++ Fill&Filter.js
@@ -0,0 +1,8 @@
+// FIll
+var array =[2,4,5,6,7,9];
+console.log(array.fill("empty"));
+
+// Filter
+const myNumbers = [23,24,25,26,27,28,29,30];
+const result = myNumbers.filter((number) => (number !=25));
+console.log(result);
