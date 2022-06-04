b7760f769e5d5390a2f5ec42021a7367290a7dac
Saksham <saksham16dec@gmail.com>
2022-06-04 21:37:52 +0530

    Adding for-in-of js

---
+++ forin-of.js
@@ -0,0 +1,10 @@
+const socials=["Instagram","Twitter","LinkedIn","Whatsapp","Facebook"];
+// for (const iterator of socials) {console.log(iterator)}
+
+const symbols ={
+    yt:"Youtube",
+    fb:"Facebook",
+    lco:"LearnCodeOnline",
+    ln:"LinkedIn"
+}
+for (const iterator in symbols){console.log(symbols[iterator]);}
