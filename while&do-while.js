31fe9ce21fe769990af679ed56a097be0276abe6
Saksham <saksham16dec@gmail.com>
2022-06-04 21:11:58 +0530

    Adding While and Do-while

---
+++ while&do-while.js
@@ -0,0 +1,17 @@
+const myStates=[
+    "Delhi",
+    "UttarPradesh",
+    "RAJASTHAN",
+    1947
+];
+let index=0;
+// while (index<myStates.length) {
+    //     const element=myStates[index];
+//     console.log(myStates[index]);
+//     index++;
+// }
+do{ //!do-while loop runs first iteration without checking for conditionals
+    const element=myStates[index];
+    console.log(myStates[index]);
+    index++;
+}while (index<myStates.length) 
