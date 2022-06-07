695e68b5c5b8aecd13fab539f759ae2dfeebebdf
Saksham <saksham16dec@gmail.com>
2022-06-07 19:48:30 +0530

    Adding Prototype JS

---
+++ Prototypes.js
@@ -0,0 +1,15 @@
+var User= function (firstName, courseCount){
+    this.firstName= firstName;
+    this.courseCount= courseCount;
+    this.getCourseCount= function () {
+    console.log(`Course-Count: ${this.courseCount}`);
+    };
+};
+
+User.prototype.getFirstName= function (firstName) {
+    console.log(`Name: ${this.firstName}`)
+};
+
+var Saksham=new User("Saksham",2);
+Saksham.getCourseCount();
+Saksham.getFirstName();
