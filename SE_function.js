ae0ae29ae8cd0de8a4ea393dd1ce0c4ff6d8efdd
Saksham Srivastava <saksham16dec@gmail,com>
2022-06-23 17:24:20 +0530

    Annonymous SE function js

---
+++ SE_function.js
@@ -0,0 +1,8 @@
+//Self Executing function snd functional programming
+// !Annonymous function
+(function () {
+    console.log("Hello");
+    console.log("Hello Again");
+})
+// calling function
+();
